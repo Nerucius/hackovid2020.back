e6b7b0488f00dd10f409be0bc530acadb88f6a3d
Christian Soler <solero93@gmail.com>
2020-04-11 14:10:35 +0200

    Fixed query on ShopRepository

--- src/main/java/hackovid2020/back/repository/ShopRepository.java
+++ src/main/java/hackovid2020/back/repository/ShopRepository.java
@@ -5,14 +5,22 @@ import hackovid2020.back.dao.Shop;
 
 import java.util.List;
 
+import org.hibernate.annotations.NamedQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ShopRepository extends JpaRepository<Shop, Long> {
 
-	@Query("SELECT c FROM category c INNER JOIN shop_category sc WHERE sc.shop_shop_id :shopId")
-	List<Category> findAllShopCategories(Long shopId);
-	
+    @Query(
+            value = "SELECT c FROM category c " +
+                    "INNER JOIN shop_category sc ON c.category_id = sc.shop_category_id " +
+                    "INNER JOIN shop s ON sc.shop_shop_id = s.shop_id " +
+                    "WHERE s.shop_id = :shopId",
+            nativeQuery = true
+    )
+    List<Category> findAllShopCategories(@Param("shopId") Long shopId);
+
 }
